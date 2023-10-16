7780eeb11ac58723c2e803ac0e9e270c13d3420f
Kiiiiiiiiiiiiiii <choys0199@gmail.com>
2023-10-17 00:36:00 +0900

    css 수정, 메세지 수정2

--- survey-client/src/App.vue
+++ survey-client/src/App.vue
@@ -1,18 +1,26 @@
 <template>
-    <LanguageBtn/>
-    <RouterView/>
+    <div class="app-main-container">
+        <LanguageBtn/>
+        <RouterView/>
+    </div>
 </template>
 
 <script>
 
 
-
 import LanguageBtn from "@/components/LanguageBtn.vue";
+
 export default {
     name: "App",
-    components:{LanguageBtn}
+    components: {LanguageBtn}
 }
 </script>
 
 <style scoped>
+.app-main-container {
+    overflow: hidden; /* 스크롤을 숨깁니다. */
+    position: fixed;
+    width: 100%;
+}
+
 </style>
--- survey-client/src/pages/Home.vue
+++ survey-client/src/pages/Home.vue
@@ -1,41 +1,42 @@
 <template>
-    <div class="container">
+    <div class="home-main-container">
         <Logo/>
-        <div class="start-aligned-image">
-            <div class="container-element">
-                {{ $t('name') }}: <input v-model="state.form.name"/>
-            </div>
-            <div class="container-element">
-                {{ $t('company') }}: <input v-model="state.form.company"/>
-            </div>
-            <div class="container-element">
-                {{ $t('email') }}: <input v-model="state.form.email"/>
-            </div>
-            <div class="container-element">
-                {{ $t('industry') }}:
-                <select v-model="state.form.industry">
+        <div class="grid-container">
+                {{ $t('name') }} :  <input class="input-element" v-model="state.form.name"/>
+                {{ $t('company') }} :  <input class="input-element" v-model="state.form.company"/>
+                {{ $t('email') }} :  <input class="input-element" v-model="state.form.email"/>
+                {{ $t('industry') }} :
+                <select class="input-element" v-model="state.form.industry">
                     <option>{{ $t('confectionary') }}</option>
                     <option>{{ $t('dessert') }}</option>
                     <option>{{ $t('hmr') }}</option>
                     <option>{{ $t('alternativeFood') }}</option>
                     <option>{{ $t('etc') }}</option>
                 </select>
-            </div>
         </div>
-    </div>
-    <div class="container-element-agree">
-        <p></p>
-        {{ $t('consent') }}<br>
-        <input type="radio" v-bind:value="true" v-model="selected">
-        <label for="one">{{ $t('agree') }}</label>
-        <span></span>
-        <input type="radio" v-bind:value="false" v-model="selected">
-        <label for="one">{{ $t('disagree') }}</label>
-    </div>
-    <br>
-    <div class="submit-butten">
-        <button v-if="!selected" class="btn btn-primary w-300 py-2" style="background-color: gray" >{{ $t('submit') }}</button>
-        <button v-if="selected" class="btn btn-primary w-300 py-2" style="background-color: red" @click="submit()">{{ $t('submit') }}</button>
+
+        <div class="container-element-agree">
+            <p></p>
+            {{ $t('consent') }}
+            <br>
+            <br>
+            <input type="radio" v-bind:value="true" v-model="selected">
+            <label for="one">{{ $t('agree') }}</label>
+            <span></span>
+            <input type="radio" v-bind:value="false" v-model="selected">
+            <label for="one">{{ $t('disagree') }}</label>
+        </div>
+        <br>
+        <br>
+        <div class="submit-butten">
+            <button v-if="!selected" class="btn btn-primary w-300 py-2" style="background-color: gray">{{
+                    $t('submit')
+                }}
+            </button>
+            <button v-if="selected" class="btn btn-primary w-300 py-2" style="background-color: red" @click="submit()">
+                {{ $t('submit') }}
+            </button>
+        </div>
     </div>
 </template>
 
@@ -72,37 +73,44 @@ export default {
         }
         return {state, submit}
     },
-    data(){
-      return {
-          selected: false, //radio 버튼의 디폴트 값 설정
-          true: true,
-          false: false
+    data() {
+        return {
+            selected: false, //radio 버튼의 디폴트 값 설정
+            true: true,
+            false: false
 
-      }
+        }
     }
 }
 </script>
 
 <style scoped>
-.container {
-    position: relative;
+.home-main-container {
+    overflow: hidden; /* 스크롤을 숨깁니다. */
+    position: fixed;
+    height: 100%;
+    max-height: 100%;
 }
 
-.start-aligned-image {
-
+.grid-container{
+    display: grid;
+    grid-template-columns: 1fr 1fr; /* 2x2 그리드 생성 */
+    gap: 10px; /* 그리드 아이템 간의 간격 설정 */
     text-align: center;
 }
 
-.container-element {
-    justify-content: center;
-    align-items: center;
+.input-element {
+    max-width: 80%;
 }
 
 .container-element-agree {
     justify-content: center;
     align-items: center;
     text-align: center;
+    font-size: 2vh;
+    white-space: pre-wrap; /*\n을 줄바꿈으로 치환*/
 }
+
 .submit-butten {
     justify-content: center;
     align-items: center;
