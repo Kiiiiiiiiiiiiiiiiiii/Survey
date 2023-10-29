29f5ec4f77cb6084ebbcba4594acfd196e0e539b
Kiiiiiiiiiiiiiii <choys0199@gmail.com>
2023-10-30 01:32:42 +0900

    테스트로직 삭제, 중복파일 제거 로직 수정, 중복코드 삭제

--- survey-server/src/main/java/com/example/surveyserver/controller/FileController.java
+++ survey-server/src/main/java/com/example/surveyserver/controller/FileController.java
@@ -10,6 +10,7 @@ import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.tomcat.jni.FileInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -21,10 +22,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -38,70 +36,6 @@ public class FileController {
     @Autowired
     ImageRepository imageRepository;
 
-    @PostMapping("/api/downloadZipfileTEST")
-    public void downloadZipFileTEST(@RequestParam("branchId") int branchId, HttpServletResponse response) {
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.setContentType("application/zip");
-        response.addHeader("Content-Disposition", "attachment; filename=\"namecard.zip\"");
-
-        FileOutputStream fos = null;
-        ZipOutputStream zipOut = null;
-        FileInputStream fis = null;
-
-        try {
-            zipOut = new ZipOutputStream(response.getOutputStream());
-            // DB에 저장되어 있는 파일 목록을 읽어온다.
-            List<Image> imgFileList = imageRepository.findAllByBranchId(branchId);
-            // 실제 Server에 파일들이 저장된 directory 경로를 구해온다.
-            String filePath = getFilePath(branchId);
-            // File 객체를 생성하여 List에 담는다.
-            /*List<File> fileList = imgFileList.stream().map(imgFileList -> {
-                return new File(filePath + "/" + fileInfo.get)
-            }).collect(Collectors.toList());*/
-
-            List<File> fileList = new ArrayList<>();
-            for (Image imageInfo : imgFileList) {
-                String filename = imageInfo.getName();
-                File file = new File(filePath + "/" + imageInfo.getName());
-                fileList.add(file);
-            }
-
-
-            for (File file : fileList) {
-                zipOut.putNextEntry(new ZipEntry(file.getName()));
-                fis = new FileInputStream(file);
-
-                StreamUtils.copy(fis, zipOut);
-                fis.close();
-                zipOut.closeEntry();
-            }
-            zipOut.close();
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-            try {
-                if (fis != null) fis.close();
-            } catch (IOException e1) {
-                System.out.println(e1.getMessage());/*ignore*/
-            }
-            try {
-                if (zipOut != null) zipOut.closeEntry();
-            } catch (IOException e2) {
-                System.out.println(e2.getMessage());/*ignore*/
-            }
-            try {
-                if (zipOut != null) zipOut.close();
-            } catch (IOException e3) {
-                System.out.println(e3.getMessage());/*ignore*/
-            }
-            try {
-                if (fos != null) fos.close();
-            } catch (IOException e4) {
-                System.out.println(e4.getMessage());/*ignore*/
-            }
-        }
-    }
-
     /**
      * 이미지 압축다운로드 코드
      * 작동은 하나 코드 정리 필요
@@ -111,12 +45,11 @@ public class FileController {
         Branch branch = branchRepository.findById(branchId);
         String imgPath = branch.getImgPath();
         String branchName = branch.getName();
-        String filePath = getFilePath(branchId);
 
         FileZipUtils fileZipUtils = new FileZipUtils();
         fileZipUtils.zip(imgPath, branchName);
 
-        FileSystemResource zipFile = new FileSystemResource(filePath + "/namecardimg_" + branchName + ".zip");
+        FileSystemResource zipFile = new FileSystemResource(imgPath + "/namecardimg_" + branchName + ".zip");
 
         HttpHeaders headers = new HttpHeaders();
         headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=namecardimg_" + branchName + ".zip");
@@ -125,11 +58,5 @@ public class FileController {
                 .contentType(MediaType.parseMediaType("application/zip"))
                 .body(zipFile);
     }
-
-
-    public String getFilePath(int branchId) {
-        Branch branch = branchRepository.findById(branchId);
-        String imgPath = branch.getImgPath();
-        return imgPath;
-    }
 }
+
--- survey-server/src/main/java/com/example/surveyserver/util/FileZipUtils.java
+++ survey-server/src/main/java/com/example/surveyserver/util/FileZipUtils.java
@@ -3,21 +3,30 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 public class FileZipUtils {
-    public void zipTest() {
-        String where = "C:\\Users\\JB\\Documents\\namecardimgs";
-        File file_ = new File(where);
+    /***
+     * Zip 파일 생성
+     * @param path
+     * @param branchName
+     */
+    public void zip(String path, String branchName) {
+        File file_ = new File(path);
+        String fileName = "namecardimg_" + branchName + ".zip";
+        existZip(path, fileName);
         File[] listFiles = file_.listFiles();
 
         FileOutputStream fos = null;
         ZipOutputStream zipOut = null;
         FileInputStream fis = null;
-        try {
 
-            fos = new FileOutputStream("C:\\Users\\JB\\Documents\\namecardimgs\\wow.zip");
+        try {
+            fos = new FileOutputStream(path + "/" + fileName);
             zipOut = new ZipOutputStream(fos);
 
             for(File fileToZip :  listFiles) {
@@ -50,47 +59,23 @@ public class FileZipUtils {
         }
     }
 
-    public void zip(String path, String branchName) {
-        String where = path;
-        File file_ = new File(where);
-        File[] listFiles = file_.listFiles();
+    /***
+     * 중복 zip 파일 제거
+     * @param path
+     * @param filename
+     * @return
+     */
+    public boolean existZip(String path, String filename) {
+        File zipFileName = new File(path + "/" + filename);
 
-        FileOutputStream fos = null;
-        ZipOutputStream zipOut = null;
-        FileInputStream fis = null;
-        String fileName = "/namecardimg_" + branchName + ".zip";
-        try {
-
-            fos = new FileOutputStream(path + fileName);
-            zipOut = new ZipOutputStream(fos);
-
-            for(File fileToZip :  listFiles) {
-
-                fis = new FileInputStream(fileToZip);
-                ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
-                zipOut.putNextEntry(zipEntry);
-
-                byte[] bytes = new byte[1024];
-                int length;
-                while((length = fis.read(bytes)) >= 0) {
-                    zipOut.write(bytes, 0, length);
-                }
-
-                fis.close();
-                zipOut.closeEntry();
-
+        if (zipFileName.exists()) {
+            if(zipFileName.delete()) {
+                return true;
+            } else {
+                return false;
             }
-
-            zipOut.close();
-            fos.close();
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            try { if(fis != null)fis.close(); } catch (IOException e1) {System.out.println(e1.getMessage());/*ignore*/}
-            try { if(zipOut != null)zipOut.closeEntry();} catch (IOException e2) {System.out.println(e2.getMessage());/*ignore*/}
-            try { if(zipOut != null)zipOut.close();} catch (IOException e3) {System.out.println(e3.getMessage());/*ignore*/}
-            try { if(fos != null)fos.close(); } catch (IOException e4) {System.out.println(e4.getMessage());/*ignore*/}
+        }else {
+            return false;
         }
     }
 
