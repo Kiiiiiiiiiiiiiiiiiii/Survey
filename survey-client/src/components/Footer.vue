435d2a583038af15b92ef535d11a21979d852d70
Kiiiiiiiiiiiiiii <choys0199@gmail.com>
2023-11-01 02:44:20 +0900

    Footer img 추가

---
+++ survey-client/src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+    <header class="header">
+        <div class="logo-container">
+            <img class="logo" alt="logo" src="../assets/footer.png">
+        </div>
+    </header>
+</template>
+
+<script>
+
+export default {
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+.header{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    max-width: 100%;
+    height: auto;
+}
+.logo {
+    justify-content: center;
+    align-items: center;
+    max-width: 100%;
+    height: auto;
+    text-align: center;
+}
+</style>
