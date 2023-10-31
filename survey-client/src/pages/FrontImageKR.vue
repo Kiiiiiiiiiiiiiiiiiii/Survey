950c1241d58b41e69f0ac94cf5ccebc67601af24
Kiiiiiiiiiiiiiii <choys0199@gmail.com>
2023-11-01 06:21:16 +0900

    3초 기다리지 않아도 화면 클릭시 메인으로 이동

--- survey-client/src/pages/FrontImageKR.vue
+++ survey-client/src/pages/FrontImageKR.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="logo-container">
-        <img src="../assets/front_img_kr.jpg" alt="Splash Image">
+    <div class="logo-container" @click="moveMain">
+        <img class="res_img" src="../assets/loading_img_kr.png" alt="Splash Image">
     </div>
 </template>
 
 <script>
 
+
 export default {
     name: "FrontImageKR",
+    methods: {
+        moveMain() {
+            this.$router.push('main?branchId=2');
+        }
+    },
     mounted() {
         setTimeout(() => {
             this.$router.push('main?branchId=1');
@@ -17,4 +23,16 @@ export default {
 </script>
 
 <style scoped>
+.logo-container{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100vw;
+    height: 100vh; /* 전체 화면 높이 */
+    background-color: #fff; /* 원하는 배경색으로 설정 */
+}
+.res_img{
+    max-width: 100vw;
+    max-height: 100vh;
+}
 </style>
