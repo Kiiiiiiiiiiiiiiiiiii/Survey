0563de329531e2b0c164ab92db922a259013e85c
Kiiiiiiiiiiiiiii <choys0199@gmail.com>
2023-11-02 01:12:03 +0900

    다국어 버튼 수정사항 적용

--- survey-client/src/components/LanguageBtn.vue
+++ survey-client/src/components/LanguageBtn.vue
@@ -1,18 +1,43 @@
 <template>
     <div class="locale">
-        <span @click="$i18n.locale='en'"><b>EN | </b></span>
-        <span @click="$i18n.locale='vn'"><b>VN | </b></span>
-        <span @click="$i18n.locale='ko'"><b>KR</b></span>
+        <span class="langStyle" v-show="showLangBtn" @click="changeLocale('ko')" :style="{ color: nowlocale === 'ko' ? 'red' : 'black', 'font-weight': nowlocale === 'ko' ? 'bold' : 'normal' }"> KR </span>
+        <span class="langStyle" v-show="!showLangBtn" @click="changeLocale('vn')" :style="{ color: nowlocale === 'vn' ? 'red' : 'black', 'font-weight': nowlocale === 'vn' ? 'bold' : 'normal' }">VN </span>
+        <span class="langStyle" @click="changeLocale('en')" :style="{ color: nowlocale === 'en' ? 'red' : 'black', 'font-weight': nowlocale === 'en' ? 'bold' : 'normal' }"> EN</span>
     </div>
 </template>
 <script>
-
+export default {
+    data(){
+        return {
+            korean: true,
+            nowlocale: 'en'
+        }
+    },
+    methods: {
+      changeLocale(locale) {
+          this.$i18n.locale = locale
+          this.nowlocale = locale
+      }
+    },
+    computed: {
+        showLangBtn() {
+            if (this.$route.query.branchId == 1) {
+                return true
+            } else {
+                return false
+            }
+        }
+    }
+}
 </script>
 
 <style scoped>
 .locale{
     text-align: right;
-    font-size: 5vw; /*화면 폭의 5%크기로 버튼 텍스트 조절*/
+    font-size: 4vw; /*화면 폭의 5%크기로 버튼 텍스트 조절*/
     padding: 2% 5%; /*//화면 폭 2%높이 및 5%너비로 버튼 조절*/
+}
+.langStyle{
+    font-weight: bold;
 }
 </style>
