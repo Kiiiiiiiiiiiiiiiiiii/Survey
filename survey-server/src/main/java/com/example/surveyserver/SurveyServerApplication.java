6482dab8875c1f9355f28fbac50fba2c5af9e3e4
Kiiiiiiiiiiiiiii <choys0199@gmail.com>
2023-11-06 03:44:48 +0900

    war 배포시 필요한 설정 추가

--- survey-server/src/main/java/com/example/surveyserver/SurveyServerApplication.java
+++ survey-server/src/main/java/com/example/surveyserver/SurveyServerApplication.java
@@ -2,13 +2,19 @@ package com.example.surveyserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SurveyServerApplication {
+public class SurveyServerApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(SurveyServerApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SurveyServerApplication.class);
 	}
 
 }
